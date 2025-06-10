f6806b9075e1c718f2dc59753d986a90cd1a971d
cbot918 <cbot918@gmail.com>
2025-06-10 13:33:41 +0800

    update

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.GET(
+		"/", func(c *gin.Context) {
+			c.JSON(
+				http.StatusOK, gin.H{
+					"message": "Welcome, this is a basic gin (https://github.com/gin-gonic/gin) server deployed on Zeabur (https://zeabur.com)",
+				},
+			)
+		},
+	)
+
+	panic(r.Run(":" + port))
 }
